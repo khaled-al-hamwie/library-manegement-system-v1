57fe9be557eebb3389c05fe7a54ed92cffcd2db3
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-15 01:54:16 +0300

    adding the query schema

---
+++ dist/app/schemas/query.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isQuery = void 0;
+const express_validator_1 = require("express-validator");
+const validationMessage_1 = __importDefault(require("../messages/validationMessage"));
+function isQuery(title) {
+    return [
+        (0, express_validator_1.query)(title)
+            .optional()
+            .trim()
+            .isString()
+            .withMessage(validationMessage_1.default.type("string"))
+            .isLength({ min: 3, max: 45 })
+            .withMessage(validationMessage_1.default.outOfLength(title)),
+    ];
+}
+exports.isQuery = isQuery;
---
+++ src/app/schemas/query.ts
@@ -0,0 +1,15 @@
+import { query, ValidationChain } from "express-validator";
+import validationMessage from "../messages/validationMessage";
+
+type queryType = "category" | "author" | "title";
+export function isQuery(title: queryType): ValidationChain[] {
+    return [
+        query(title)
+            .optional()
+            .trim()
+            .isString()
+            .withMessage(validationMessage.type("string"))
+            .isLength({ min: 3, max: 45 })
+            .withMessage(validationMessage.outOfLength(title)),
+    ];
+}
