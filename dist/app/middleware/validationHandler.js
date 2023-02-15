a8b08f120d5a11315e7e5be38d34bf6b973f85f8
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-15 21:46:32 +0300

    update the validation handeler to use a error formater

--- dist/app/middleware/validationHandler.js
+++ dist/app/middleware/validationHandler.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validationHandler = void 0;
 const express_validator_1 = require("express-validator");
 const responses_1 = __importDefault(require("../traits/responses"));
+const errorFormatter = ({ location, msg, param, value, nestedErrors, }) => {
+    return { field: param, error: msg };
+};
 function validationHandler(req, res, next) {
-    const errors = (0, express_validator_1.validationResult)(req);
+    const errors = (0, express_validator_1.validationResult)(req).formatWith(errorFormatter);
     if (!errors.isEmpty()) {
         return responses_1.default.validation(res, errors.array());
     }
--- src/app/middleware/validationHandler.ts
+++ src/app/middleware/validationHandler.ts
@@ -1,13 +1,28 @@
 import { NextFunction, Request, Response } from "express";
-import { validationResult } from "express-validator";
+import {
+    ErrorFormatter,
+    Result,
+    ValidationError,
+    validationResult,
+} from "express-validator";
 import HttpResponse from "../traits/responses";
+
+const errorFormatter = ({
+    location,
+    msg,
+    param,
+    value,
+    nestedErrors,
+}: ValidationError) => {
+    return { field: param, error: msg };
+};
 
 export function validationHandler(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    const errors = validationResult(req);
+    const errors = validationResult(req).formatWith(errorFormatter);
     if (!errors.isEmpty()) {
         return HttpResponse.validation(res, errors.array());
     }
