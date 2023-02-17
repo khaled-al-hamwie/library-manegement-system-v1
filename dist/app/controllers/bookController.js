54128d18ee57e00961683083b2a8e2863ae3dc54
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-17 22:43:16 +0300

    adding the create method

--- dist/app/controllers/bookController.js
+++ dist/app/controllers/bookController.js
@@ -1,7 +1,4 @@
 "use strict";
-// import { NextFunction, Request, Response } from "express";
-// import { Book } from "../models/book";
-// import sequelize from "../providers/databaseProvider";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,21 +8,99 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const sequelize_1 = require("sequelize");
+const book_1 = require("../models/book");
+const responses_1 = __importDefault(require("../traits/responses"));
 class BookController {
     static getBooks(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            const { title, category, author } = req.query;
+            if (!title)
+                return responses_1.default.fetch(res, yield book_1.Book.findAll());
+            else {
+                return responses_1.default.fetch(res, yield book_1.Book.findAll({
+                    where: { name: { [sequelize_1.Op.regexp]: title } },
+                }));
+            }
         });
     }
     static createBook(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            const imageName = req.file
+                ? Date.now() +
+                    "-" +
+                    Math.round(Math.random() * 1e9) +
+                    "-" +
+                    req.file.originalname.trim()
+                : null;
+            yield book_1.Book.create({
+                category_id: req.body.category_id,
+                author_id: req.body.author_id,
+                publisher_id: req.body.publisher_id,
+                status_id: req.body.status_id,
+                title: req.body.title,
+                description: req.body.description,
+                edition: req.body.edition,
+                price: req.body.price,
+                reservation_price: req.body.reservation_price,
+                image: imageName,
+            })
+                .then((result) => {
+                if (req.file && imageName) {
+                    {
+                        let buffer = req.file.buffer;
+                        fs_1.default.writeFile(path_1.default.join("images", imageName), buffer, (err) => {
+                            if (err)
+                                throw err;
+                            console.log("The file has been saved!");
+                        });
+                    }
+                }
+                return responses_1.default.creation(res, result, "book");
+            })
+                .catch((err) => responses_1.default.server(res, err));
         });
     }
     static showBook(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            const id = req.params.id;
+            const book = yield book_1.Book.findByPk(id);
+            if (book) {
+                const publisher = yield book.getPublisher();
+                return responses_1.default.fetch(res, {
+                    book_id: book.book_id,
+                    category: {
+                        category_id: book.category_id,
+                        name: (yield book.getCategory()).name,
+                    },
+                    author: {
+                        author_id: book.author_id,
+                        name: (yield book.getAuthor()).name,
+                    },
+                    publisher: {
+                        publisher_id: book.publisher_id,
+                        name: publisher.name,
+                        year: publisher.publishing_date,
+                    },
+                    status: {
+                        status_id: book.status_id,
+                        name: (yield book.getStatus()).name,
+                    },
+                    title: book.title,
+                    description: book.description,
+                    edition: book.edition,
+                    price: book.price,
+                    // reservation_price: book.reservation_daily_value,
+                    image: book.image,
+                });
+            }
+            return responses_1.default.notFound(res, "category", id);
         });
     }
     static updateBook(req, res, next) {
--- src/app/controllers/bookController.ts
+++ src/app/controllers/bookController.ts
@@ -1,39 +1,65 @@
-// import { NextFunction, Request, Response } from "express";
-// import { Book } from "../models/book";
-// import sequelize from "../providers/databaseProvider";
-
-// class BookController {
-//     static async getBooks(
-//         req: Request,
-//         res: Response,
-//         next: NextFunction
-//     ): Promise<Response> {
-//         return res.json({
-//             test: await Book(sequelize).findAll(),
-//         });
-//     }
-// }
-// export default BookController;
-// // getBooks addBook showBook updateBook deleteBook
+import { json } from "body-parser";
 import { NextFunction, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import { Op } from "sequelize";
 import { Book } from "../models/book";
-import sequelize from "../providers/databaseProvider";
-
+import HttpResponse from "../traits/responses";
 class BookController {
     static async getBooks(
         req: Request,
         res: Response,
         next: NextFunction
     ): Promise<Response> {
-        return res.json();
+        const { title, category, author } = req.query;
+        if (!title) return HttpResponse.fetch(res, await Book.findAll());
+        else {
+            return HttpResponse.fetch(
+                res,
+                await Book.findAll({
+                    where: { name: { [Op.regexp]: title } },
+                })
+            );
+        }
     }
 
-    static async createBook(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response> {
-        return res.json();
+    static async createBook(req: Request, res: Response, next: NextFunction) {
+        const imageName: string | null = req.file
+            ? Date.now() +
+              "-" +
+              Math.round(Math.random() * 1e9) +
+              "-" +
+              req.file.originalname.trim()
+            : null;
+        await Book.create({
+            category_id: req.body.category_id,
+            author_id: req.body.author_id,
+            publisher_id: req.body.publisher_id,
+            status_id: req.body.status_id,
+            title: req.body.title,
+            description: req.body.description,
+            edition: req.body.edition,
+            price: req.body.price,
+            reservation_price: req.body.reservation_price,
+            image: imageName,
+        })
+            .then((result) => {
+                if (req.file && imageName) {
+                    {
+                        let buffer = req.file.buffer;
+                        fs.writeFile(
+                            path.join("images", imageName),
+                            buffer,
+                            (err) => {
+                                if (err) throw err;
+                                console.log("The file has been saved!");
+                            }
+                        );
+                    }
+                }
+                return HttpResponse.creation(res, result, "book");
+            })
+            .catch((err) => HttpResponse.server(res, err));
     }
 
     static async showBook(
@@ -41,7 +67,38 @@ class BookController {
         res: Response,
         next: NextFunction
     ): Promise<Response> {
-        return res.json();
+        const id: string = req.params.id;
+        const book = await Book.findByPk(id);
+        if (book) {
+            const publisher = await book.getPublisher();
+            return HttpResponse.fetch(res, {
+                book_id: book.book_id,
+                category: {
+                    category_id: book.category_id,
+                    name: (await book.getCategory()).name,
+                },
+                author: {
+                    author_id: book.author_id,
+                    name: (await book.getAuthor()).name,
+                },
+                publisher: {
+                    publisher_id: book.publisher_id,
+                    name: publisher.name,
+                    year: publisher.publishing_date,
+                },
+                status: {
+                    status_id: book.status_id,
+                    name: (await book.getStatus()).name,
+                },
+                title: book.title,
+                description: book.description,
+                edition: book.edition,
+                price: book.price,
+                // reservation_price: book.reservation_daily_value,
+                image: book.image,
+            });
+        }
+        return HttpResponse.notFound(res, "category", id);
     }
 
     static async updateBook(
