faceb18b0e93562f78421d43ab38d320416001d0
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-17 22:42:57 +0300

    adding the image validation

--- dist/routes/bookRoute.js
+++ dist/routes/bookRoute.js
@@ -4,15 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const multer_1 = __importDefault(require("multer"));
 const bookController_1 = __importDefault(require("../app/controllers/bookController"));
 const validationHandler_1 = require("../app/middleware/validationHandler");
 const id_1 = require("../app/schemas/id");
+const image_1 = require("../app/schemas/image");
 const bookValidator_1 = require("../app/validators/bookValidator");
 const bookRouter = (0, express_1.Router)();
 bookRouter
     .route("/book")
     .get((0, bookValidator_1.bookValidator)("get"), validationHandler_1.validationHandler, bookController_1.default.getBooks)
-    .post((0, bookValidator_1.bookValidator)("create"), validationHandler_1.validationHandler, bookController_1.default.createBook);
+    .post((0, multer_1.default)().single("image"), (0, bookValidator_1.bookValidator)("create"), image_1.image, validationHandler_1.validationHandler, bookController_1.default.createBook);
 bookRouter
     .route("/book/:id")
     .all([...id_1.id], validationHandler_1.validationHandler)
--- src/routes/bookRoute.ts
+++ src/routes/bookRoute.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import multer from "multer";
+import path from "path";
 import BookController from "../app/controllers/bookController";
 import { validationHandler } from "../app/middleware/validationHandler";
 import { id } from "../app/schemas/id";
+import { image } from "../app/schemas/image";
 import { bookValidator } from "../app/validators/bookValidator";
 const bookRouter: Router = Router();
 
@@ -9,7 +12,9 @@ bookRouter
     .route("/book")
     .get(bookValidator("get"), validationHandler, BookController.getBooks)
     .post(
+        multer().single("image"),
         bookValidator("create"),
+        image,
         validationHandler,
         BookController.createBook
     );
