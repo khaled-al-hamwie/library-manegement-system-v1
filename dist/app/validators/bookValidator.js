c1dc9d535784134121a3e6010049e83b2b7fd40a
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-17 21:46:19 +0300

    rearrenging the validations

--- dist/app/validators/bookValidator.js
+++ dist/app/validators/bookValidator.js
@@ -8,27 +8,27 @@ const string_1 = require("../schemas/string");
 function bookValidator(type = "get") {
     if (type == "create")
         return [
+            ...(0, forignKey_1.forignKey)("category_id"),
+            ...(0, forignKey_1.forignKey)("author_id"),
+            ...(0, forignKey_1.forignKey)("publisher_id"),
+            ...(0, forignKey_1.forignKey)("status_id"),
             ...(0, string_1.string)("title"),
             ...(0, string_1.string)("description"),
             ...(0, string_1.stringO)("edition"),
             ...(0, price_1.price)("price"),
             ...(0, price_1.price)("reservation_daily_value"),
-            ...(0, forignKey_1.forignKey)("category_id"),
-            ...(0, forignKey_1.forignKey)("author_id"),
-            ...(0, forignKey_1.forignKey)("publisher_id"),
-            ...(0, forignKey_1.forignKey)("status_id"),
         ];
     if (type == "update")
         return [
+            ...(0, forignKey_1.forignKeyO)("category_id"),
+            ...(0, forignKey_1.forignKeyO)("author_id"),
+            ...(0, forignKey_1.forignKeyO)("publisher_id"),
+            ...(0, forignKey_1.forignKeyO)("status_id"),
             ...(0, string_1.string)("title"),
             ...(0, string_1.string)("description"),
             ...(0, string_1.stringO)("edition"),
             ...(0, price_1.priceO)("price"),
             ...(0, price_1.priceO)("reservation_daily_value"),
-            ...(0, forignKey_1.forignKeyO)("category_id"),
-            ...(0, forignKey_1.forignKeyO)("author_id"),
-            ...(0, forignKey_1.forignKeyO)("publisher_id"),
-            ...(0, forignKey_1.forignKeyO)("status_id"),
         ];
     return [...(0, query_1.isQuery)("title"), ...(0, query_1.isQuery)("author"), ...(0, query_1.isQuery)("category")];
 }
--- src/app/validators/bookValidator.ts
+++ src/app/validators/bookValidator.ts
@@ -1,5 +1,6 @@
 import { ValidationChain } from "express-validator";
 import { forignKey, forignKeyO } from "../schemas/forignKey";
+import { image } from "../schemas/image";
 import { price, priceO } from "../schemas/price";
 import { isQuery } from "../schemas/query";
 import { string, stringO } from "../schemas/string";
@@ -9,27 +10,27 @@ export function bookValidator(
 ): ValidationChain[] {
     if (type == "create")
         return [
+            ...forignKey("category_id"),
+            ...forignKey("author_id"),
+            ...forignKey("publisher_id"),
+            ...forignKey("status_id"),
             ...string("title"),
             ...string("description"),
             ...stringO("edition"),
             ...price("price"),
             ...price("reservation_daily_value"),
-            ...forignKey("category_id"),
-            ...forignKey("author_id"),
-            ...forignKey("publisher_id"),
-            ...forignKey("status_id"),
         ];
     if (type == "update")
         return [
+            ...forignKeyO("category_id"),
+            ...forignKeyO("author_id"),
+            ...forignKeyO("publisher_id"),
+            ...forignKeyO("status_id"),
             ...string("title"),
             ...string("description"),
             ...stringO("edition"),
             ...priceO("price"),
             ...priceO("reservation_daily_value"),
-            ...forignKeyO("category_id"),
-            ...forignKeyO("author_id"),
-            ...forignKeyO("publisher_id"),
-            ...forignKeyO("status_id"),
         ];
     return [...isQuery("title"), ...isQuery("author"), ...isQuery("category")];
 }
