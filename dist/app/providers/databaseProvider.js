e7e53f72bf1e2762d89ff1ffee5e47402ab77084
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-19 20:11:12 +0300

    removing the sql query loging

--- dist/app/providers/databaseProvider.js
+++ dist/app/providers/databaseProvider.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 require("../providers/envProvider");
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { dialect: "mysql", host: process.env.DB_HOST });
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { dialect: "mysql", host: process.env.DB_HOST, logging: false });
 exports.default = sequelize;
--- src/app/providers/databaseProvider.ts
+++ src/app/providers/databaseProvider.ts
@@ -4,6 +4,6 @@ const sequelize: Sequelize = new Sequelize(
     process.env.DB_NAME!,
     process.env.DB_USER!,
     process.env.DB_PASSWORD,
-    { dialect: "mysql", host: process.env.DB_HOST }
+    { dialect: "mysql", host: process.env.DB_HOST, logging: false }
 );
 export default sequelize;
