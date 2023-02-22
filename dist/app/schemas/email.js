9483047f1bccb9ad683292ccb313bc7fdc19bbce
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-02-22 22:02:12 +0300

    update the email schema to check if the email is unique

--- dist/app/schemas/email.js
+++ dist/app/schemas/email.js
@@ -6,12 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.email = void 0;
 const express_validator_1 = require("express-validator");
 const validationMessage_1 = __importDefault(require("../messages/validationMessage"));
+const credential_1 = require("../models/credential");
 function email() {
     return [
         (0, express_validator_1.body)("email")
             .trim()
             .isEmail({ allow_ip_domain: false, allow_utf8_local_part: false })
-            .withMessage((value) => validationMessage_1.default.type("email", value)),
+            .withMessage((value) => validationMessage_1.default.type("email", value))
+            .bail()
+            .custom((value, { req, location, path }) => {
+            return credential_1.Credential.findOne({
+                where: { email: value },
+            }).then((email) => {
+                if (email) {
+                    return Promise.reject("you can't use this email");
+                }
+            });
+        }),
     ];
 }
 exports.email = email;
+//check if email exists
--- src/app/schemas/email.ts
+++ src/app/schemas/email.ts
@@ -1,11 +1,23 @@
 import { body, ValidationChain } from "express-validator";
 import validationMessage from "../messages/validationMessage";
+import { Credential } from "../models/credential";
 
 export function email(): ValidationChain[] {
     return [
         body("email")
             .trim()
             .isEmail({ allow_ip_domain: false, allow_utf8_local_part: false })
-            .withMessage((value) => validationMessage.type("email", value)),
+            .withMessage((value) => validationMessage.type("email", value))
+            .bail()
+            .custom((value, { req, location, path }) => {
+                return Credential.findOne({
+                    where: { email: value },
+                }).then((email) => {
+                    if (email) {
+                        return Promise.reject("you can't use this email");
+                    }
+                });
+            }),
     ];
 }
+//check if email exists
