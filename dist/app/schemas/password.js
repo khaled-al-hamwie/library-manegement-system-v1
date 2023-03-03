d1b8823f3d9ea2fd3fc75cea76b803e3ee2caf97
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-03 21:00:42 +0300

    validation the password on log in

--- dist/app/schemas/password.js
+++ dist/app/schemas/password.js
@@ -6,20 +6,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.password = void 0;
 const express_validator_1 = require("express-validator");
 const validationMessage_1 = __importDefault(require("../messages/validationMessage"));
-function password() {
-    return [
-        (0, express_validator_1.body)("password")
-            .trim()
-            .isLength({ min: 20, max: 50 })
-            .withMessage(validationMessage_1.default.outOfLength("password")),
-        (0, express_validator_1.body)("confirm_password")
-            .trim()
-            .custom((input, { req, location, path }) => {
-            if (input !== req.body.password) {
-                throw new Error(`the password must match`);
-            }
-            return true;
-        }),
-    ];
+function password(withConfirmation) {
+    if (withConfirmation)
+        return [
+            (0, express_validator_1.body)("password")
+                .trim()
+                .notEmpty()
+                .withMessage(validationMessage_1.default.empty("password"))
+                .bail()
+                .isLength({ min: 20, max: 50 })
+                .withMessage(validationMessage_1.default.outOfLength("password")),
+            (0, express_validator_1.body)("confirm_password")
+                .trim()
+                .custom((input, { req, location, path }) => {
+                if (input !== req.body.password) {
+                    throw new Error(`the password must match`);
+                }
+                return true;
+            }),
+        ];
+    else
+        return [
+            (0, express_validator_1.body)("password")
+                .trim()
+                .notEmpty()
+                .withMessage(validationMessage_1.default.empty("password"))
+                .bail()
+                .isLength({ min: 20, max: 50 })
+                .withMessage(validationMessage_1.default.outOfLength("password")),
+        ];
 }
 exports.password = password;
--- src/app/schemas/password.ts
+++ src/app/schemas/password.ts
@@ -1,19 +1,33 @@
 import { body, ValidationChain } from "express-validator";
 import validationMessage from "../messages/validationMessage";
 
-export function password(): ValidationChain[] {
-    return [
-        body("password")
-            .trim()
-            .isLength({ min: 20, max: 50 })
-            .withMessage(validationMessage.outOfLength("password")),
-        body("confirm_password")
-            .trim()
-            .custom((input, { req, location, path }) => {
-                if (input !== req.body.password) {
-                    throw new Error(`the password must match`);
-                }
-                return true;
-            }),
-    ];
+export function password(withConfirmation: boolean): ValidationChain[] {
+    if (withConfirmation)
+        return [
+            body("password")
+                .trim()
+                .notEmpty()
+                .withMessage(validationMessage.empty("password"))
+                .bail()
+                .isLength({ min: 20, max: 50 })
+                .withMessage(validationMessage.outOfLength("password")),
+            body("confirm_password")
+                .trim()
+                .custom((input, { req, location, path }) => {
+                    if (input !== req.body.password) {
+                        throw new Error(`the password must match`);
+                    }
+                    return true;
+                }),
+        ];
+    else
+        return [
+            body("password")
+                .trim()
+                .notEmpty()
+                .withMessage(validationMessage.empty("password"))
+                .bail()
+                .isLength({ min: 20, max: 50 })
+                .withMessage(validationMessage.outOfLength("password")),
+        ];
 }
