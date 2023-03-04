9b0eee37414a19935bfb0f3b2f5d1631e9305437
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-05 01:45:40 +0300

    adding the login route

--- dist/routes/publicRoute.js
+++ dist/routes/publicRoute.js
@@ -1,9 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const registerController_1 = __importDefault(require("../app/controllers/registerController"));
+const validationHandler_1 = require("../app/middleware/validationHandler");
+const registerValidator_1 = require("../app/validators/registerValidator");
 const authorRoute_1 = require("./sub-routes/authorRoute");
 const bookRoute_1 = require("./sub-routes/bookRoute");
 const categoryRoute_1 = require("./sub-routes/categoryRoute");
 const publicRouter = (0, express_1.Router)();
+publicRouter
+    .route("/login")
+    .post((0, registerValidator_1.loginValidator)(), validationHandler_1.validationHandler, registerController_1.default.login);
 publicRouter.use(authorRoute_1.authorRouterPublic, categoryRoute_1.categoryRouterPublic, bookRoute_1.bookRouterPublic);
 exports.default = publicRouter;
--- src/routes/publicRoute.ts
+++ src/routes/publicRoute.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import RegisterController from "../app/controllers/registerController";
+import { validationHandler } from "../app/middleware/validationHandler";
+import { loginValidator } from "../app/validators/registerValidator";
 import { authorRouterPublic } from "./sub-routes/authorRoute";
 import { bookRouterPublic } from "./sub-routes/bookRoute";
 import { categoryRouterPublic } from "./sub-routes/categoryRoute";
 
 const publicRouter: Router = Router();
-
+publicRouter
+    .route("/login")
+    .post(loginValidator(), validationHandler, RegisterController.login);
 publicRouter.use(authorRouterPublic, categoryRouterPublic, bookRouterPublic);
 
 export default publicRouter;
