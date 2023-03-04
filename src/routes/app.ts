52ed77f45d1bbe0b5fd238566b5c85f99e894190
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-04 21:10:32 +0300

    adding the staff and public route

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,27 +1,19 @@
 import bodyParser from "body-parser";
 import { json, Router } from "express";
 import { errorHandler } from "../app/middleware/errorHandler";
-import authorRouter from "./authorRoute";
-import bookRouter from "./bookRoute";
-import categoryRouter from "./categoryRoute";
-import publisherRouter from "./publisherRoute";
+import publicRouter from "./publicRoute";
 import readerRouter from "./readerRoute";
-import statusRouter from "./statusRouter";
+import staffRouter from "./staffRoute";
 const router: Router = Router();
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 // router.use(json());
 
-router.use(
-    "/admin",
-    categoryRouter,
-    authorRouter,
-    publisherRouter,
-    statusRouter,
-    bookRouter
-);
+router.use(publicRouter);
+router.use(staffRouter);
 
 router.use(readerRouter);
+
 router.use(errorHandler);
 export default router;
