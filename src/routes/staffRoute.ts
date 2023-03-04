ba835bc3331906df3f09d39946a5590642eca549
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-04 21:12:50 +0300

    adding the register and get staff and delete a staff

--- src/routes/staffRoute.ts
+++ src/routes/staffRoute.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import StaffController from "../app/controllers/staffController";
+import { validationHandler } from "../app/middleware/validationHandler";
+import { registerValidator } from "../app/validators/registerValidator";
 import { authorRouterAdmin } from "./sub-routes/authorRoute";
 import { bookRouterAdmin } from "./sub-routes/bookRoute";
 import { categoryRouterAdmin } from "./sub-routes/categoryRoute";
@@ -7,6 +10,15 @@ import { statusRouterAdmin } from "./sub-routes/statusRouter";
 
 const staffRouter: Router = Router();
 
+staffRouter.post(
+    "/admin/register",
+    registerValidator(),
+    validationHandler,
+    StaffController.createStaff
+);
+
+staffRouter.get("/admin/staff", StaffController.getStaff);
+staffRouter.delete("/admin/staff/:id", StaffController.deleteStaff);
 staffRouter.use(
     "/admin",
     authorRouterAdmin,
