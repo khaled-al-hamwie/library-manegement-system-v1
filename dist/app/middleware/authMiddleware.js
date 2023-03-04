553e43d53afa6e5385f116c5586837a0ceab9c8d
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-05 01:44:53 +0300

    adding user middleware

--- dist/app/middleware/authMiddleware.js
+++ dist/app/middleware/authMiddleware.js
@@ -3,20 +3,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.authMiddleware = void 0;
+exports.userMiddleware = exports.adminMiddleware = void 0;
 const jsonwebtoken_1 = require("jsonwebtoken");
 const responses_1 = __importDefault(require("../traits/responses"));
-function authMiddleware(req, res, next) {
+function adminMiddleware(req, res, next) {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
         (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET, function (err, decoded) {
             if (err)
                 return responses_1.default.Unauthorized(res);
             if (decoded && decoded.isAdmin)
+                next();
+            else
+                return responses_1.default.Unauthorized(res);
+        });
+    }
+    else
+        return responses_1.default.Unauthorized(res);
+}
+exports.adminMiddleware = adminMiddleware;
+function userMiddleware(req, res, next) {
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(" ")[1];
+        (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET, function (err, decoded) {
+            if (err)
+                return responses_1.default.Unauthorized(res);
+            if (decoded && !decoded.isAdmin)
                 next(); // bar
         });
     }
     else
         return responses_1.default.Unauthorized(res);
 }
-exports.authMiddleware = authMiddleware;
+exports.userMiddleware = userMiddleware;
--- src/app/middleware/authMiddleware.ts
+++ src/app/middleware/authMiddleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import HttpResponse from "../traits/responses";
 
-export function authMiddleware(
+export function adminMiddleware(
     req: Request,
     res: Response,
     next: NextFunction
@@ -11,7 +11,21 @@ export function authMiddleware(
         const token = req.headers.authorization.split(" ")[1];
         verify(token, process.env.JWT_SECRET!, function (err, decoded: any) {
             if (err) return HttpResponse.Unauthorized(res);
-            if (decoded && decoded.isAdmin) next(); // bar
+            if (decoded && decoded.isAdmin) next();
+            else return HttpResponse.Unauthorized(res);
+        });
+    } else return HttpResponse.Unauthorized(res);
+}
+export function userMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(" ")[1];
+        verify(token, process.env.JWT_SECRET!, function (err, decoded: any) {
+            if (err) return HttpResponse.Unauthorized(res);
+            if (decoded && !decoded.isAdmin) next(); // bar
         });
     } else return HttpResponse.Unauthorized(res);
 }
