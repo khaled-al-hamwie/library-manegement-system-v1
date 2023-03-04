f445b9ead5177c0a32ea688f0352fdf234feb127
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-04 22:15:13 +0300

    adding authmiddleware

--- src/routes/staffRoute.ts
+++ src/routes/staffRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import StaffController from "../app/controllers/staffController";
+import { authMiddleware } from "../app/middleware/authMiddleware";
 import { validationHandler } from "../app/middleware/validationHandler";
 import { registerValidator } from "../app/validators/registerValidator";
 import { authorRouterAdmin } from "./sub-routes/authorRoute";
@@ -12,15 +13,21 @@ const staffRouter: Router = Router();
 
 staffRouter.post(
     "/admin/register",
+    authMiddleware,
     registerValidator(),
     validationHandler,
     StaffController.createStaff
 );
 
-staffRouter.get("/admin/staff", StaffController.getStaff);
-staffRouter.delete("/admin/staff/:id", StaffController.deleteStaff);
+staffRouter.get("/admin/staff", authMiddleware, StaffController.getStaff);
+staffRouter.delete(
+    "/admin/staff/:id",
+    authMiddleware,
+    StaffController.deleteStaff
+);
 staffRouter.use(
     "/admin",
+    authMiddleware,
     authorRouterAdmin,
     categoryRouterAdmin,
     bookRouterAdmin,
