be85d9943d082fb7a107b5af0ef865e71fc191de
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-04 16:47:17 +0300

    moving the status router to sub routers

--- dist/routes/sub-routes/statusRouter.js
+++ dist/routes/sub-routes/statusRouter.js
@@ -3,21 +3,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.statusRouterPublic = exports.statusRouterAdmin = void 0;
 const express_1 = require("express");
 const express_validator_1 = require("express-validator");
-const statusController_1 = __importDefault(require("../app/controllers/statusController"));
-const validationHandler_1 = require("../app/middleware/validationHandler");
-const id_1 = require("../app/schemas/id");
-const statusValidator_1 = require("../app/validators/statusValidator");
-const statusRouter = (0, express_1.Router)();
-statusRouter
+const statusController_1 = __importDefault(require("../../app/controllers/statusController"));
+const validationHandler_1 = require("../../app/middleware/validationHandler");
+const id_1 = require("../../app/schemas/id");
+const statusValidator_1 = require("../../app/validators/statusValidator");
+exports.statusRouterAdmin = (0, express_1.Router)();
+exports.statusRouterPublic = (0, express_1.Router)();
+exports.statusRouterAdmin
     .route("/status")
     .get((0, express_validator_1.param)("name"), validationHandler_1.validationHandler, statusController_1.default.getStatus)
     .post((0, statusValidator_1.statusValidator)(), validationHandler_1.validationHandler, statusController_1.default.createStatus);
-statusRouter
+exports.statusRouterAdmin
     .route("/status/:id")
     .all(id_1.id, validationHandler_1.validationHandler)
     .get(statusController_1.default.showStatus)
     .patch((0, statusValidator_1.statusValidator)(true), validationHandler_1.validationHandler, statusController_1.default.updateStatus)
     .delete(statusController_1.default.deleteStatus);
-exports.default = statusRouter;
+exports.statusRouterPublic
+    .route("/status")
+    .get((0, express_validator_1.param)("name"), validationHandler_1.validationHandler, statusController_1.default.getStatus);
+exports.statusRouterPublic
+    .route("/status/:id")
+    .all(id_1.id, validationHandler_1.validationHandler)
+    .get(statusController_1.default.showStatus);
---
+++ src/routes/sub-routes/statusRouter.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { param } from "express-validator";
+import StatusController from "../../app/controllers/statusController";
+import { validationHandler } from "../../app/middleware/validationHandler";
+import { id } from "../../app/schemas/id";
+import { statusValidator } from "../../app/validators/statusValidator";
+export const statusRouterAdmin: Router = Router();
+export const statusRouterPublic: Router = Router();
+
+statusRouterAdmin
+    .route("/status")
+    .get(param("name"), validationHandler, StatusController.getStatus)
+    .post(statusValidator(), validationHandler, StatusController.createStatus);
+statusRouterAdmin
+    .route("/status/:id")
+    .all(id, validationHandler)
+    .get(StatusController.showStatus)
+    .patch(
+        statusValidator(true),
+        validationHandler,
+        StatusController.updateStatus
+    )
+    .delete(StatusController.deleteStatus);
+
+statusRouterPublic
+    .route("/status")
+    .get(param("name"), validationHandler, StatusController.getStatus);
+
+statusRouterPublic
+    .route("/status/:id")
+    .all(id, validationHandler)
+    .get(StatusController.showStatus);
