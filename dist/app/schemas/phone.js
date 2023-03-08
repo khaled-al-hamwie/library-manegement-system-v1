1c219cd5c9283f3823aecc144f983f61f872c48a
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-08 13:42:11 +0300

    adding the phone optional validation

--- dist/app/schemas/phone.js
+++ dist/app/schemas/phone.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.phone = void 0;
+exports.phoneO = exports.phone = void 0;
 const express_validator_1 = require("express-validator");
 const validationMessage_1 = __importDefault(require("../messages/validationMessage"));
 function phone() {
@@ -11,7 +11,17 @@ function phone() {
         (0, express_validator_1.body)("phone_number")
             .trim()
             .isMobilePhone("any")
-            .withMessage((value) => validationMessage_1.default.type("email", value)),
+            .withMessage((value) => validationMessage_1.default.type("phone", value)),
     ];
 }
 exports.phone = phone;
+function phoneO() {
+    return [
+        (0, express_validator_1.body)("phone_number")
+            .optional()
+            .trim()
+            .isMobilePhone("any")
+            .withMessage((value) => validationMessage_1.default.type("phone", value)),
+    ];
+}
+exports.phoneO = phoneO;
--- src/app/schemas/phone.ts
+++ src/app/schemas/phone.ts
@@ -6,6 +6,15 @@ export function phone(): ValidationChain[] {
         body("phone_number")
             .trim()
             .isMobilePhone("any")
-            .withMessage((value) => validationMessage.type("email", value)),
+            .withMessage((value) => validationMessage.type("phone", value)),
+    ];
+}
+export function phoneO(): ValidationChain[] {
+    return [
+        body("phone_number")
+            .optional()
+            .trim()
+            .isMobilePhone("any")
+            .withMessage((value) => validationMessage.type("phone", value)),
     ];
 }
