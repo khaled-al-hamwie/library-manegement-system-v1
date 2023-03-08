05cf40901c248880e66ab284c252f4b627863c32
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-08 13:31:06 +0300

    update the reader auth to check if the credential exists

--- dist/app/middleware/authMiddleware.js
+++ dist/app/middleware/authMiddleware.js
@@ -1,10 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userMiddleware = exports.adminMiddleware = void 0;
 const jsonwebtoken_1 = require("jsonwebtoken");
+const credential_1 = require("../models/credential");
 const responses_1 = __importDefault(require("../traits/responses"));
 function adminMiddleware(req, res, next) {
     if (req.headers.authorization) {
@@ -26,10 +36,17 @@ function userMiddleware(req, res, next) {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
         (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET, function (err, decoded) {
-            if (err)
-                return responses_1.default.Unauthorized(res);
-            if (decoded && !decoded.isAdmin)
-                next(); // bar
+            return __awaiter(this, void 0, void 0, function* () {
+                if (err)
+                    return responses_1.default.Unauthorized(res);
+                if (decoded && !decoded.isAdmin) {
+                    const credential = yield credential_1.Credential.findByPk(decoded.id);
+                    if (!credential || !credential.tokens.includes(token))
+                        return responses_1.default.Unauthorized(res);
+                    req.body.id = decoded.id;
+                    next();
+                }
+            });
         });
     }
     else
--- src/app/middleware/authMiddleware.ts
+++ src/app/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
+import { Credential } from "../models/credential";
 import HttpResponse from "../traits/responses";
 
 export function adminMiddleware(
@@ -23,9 +24,19 @@ export function userMiddleware(
 ) {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
-        verify(token, process.env.JWT_SECRET!, function (err, decoded: any) {
-            if (err) return HttpResponse.Unauthorized(res);
-            if (decoded && !decoded.isAdmin) next(); // bar
-        });
+        verify(
+            token,
+            process.env.JWT_SECRET!,
+            async function (err, decoded: any) {
+                if (err) return HttpResponse.Unauthorized(res);
+                if (decoded && !decoded.isAdmin) {
+                    const credential = await Credential.findByPk(decoded.id);
+                    if (!credential || !credential.tokens.includes(token))
+                        return HttpResponse.Unauthorized(res);
+                    req.body.id = decoded.id;
+                    next();
+                }
+            }
+        );
     } else return HttpResponse.Unauthorized(res);
 }
