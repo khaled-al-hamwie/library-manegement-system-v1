c4e836ae96fbb35ec6d6a0418ca0f37b0ad89dee
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-09 18:31:07 +0300

    adding the get payment

--- dist/app/controllers/paymentController.js
+++ dist/app/controllers/paymentController.js
@@ -12,6 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
 const payment_1 = require("../models/payment");
 const staff_1 = require("../models/staff");
 const responses_1 = __importDefault(require("../traits/responses"));
@@ -19,16 +20,76 @@ const bookController_1 = __importDefault(require("./bookController"));
 class PaymentController {
     static getPayment(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            let option = req.query.option;
+            let searchQuery = undefined;
+            let start = new Date();
+            let end = new Date();
+            switch (option) {
+                case "today":
+                    start.setHours(0, 0, 0, 0);
+                    end.setHours(23, 59, 59, 59);
+                    searchQuery = {
+                        where: {
+                            payment_date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "yesterday":
+                    start.setHours(0, 0, 0, 0);
+                    start.setDate(start.getDate() - 1);
+                    end.setHours(23, 59, 59, 59);
+                    end.setDate(end.getDate() - 1);
+                    searchQuery = {
+                        where: {
+                            payment_date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "this_week":
+                    start.setHours(0, 0, 0, 0);
+                    end.setHours(23, 59, 59, 59);
+                    end.setDate(end.getDate() + 7);
+                    searchQuery = {
+                        where: {
+                            payment_date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "last_week":
+                    start.setHours(0, 0, 0, 0);
+                    end.setDate(end.getDate() - 7);
+                    end.setHours(23, 59, 59, 59);
+                    searchQuery = {
+                        where: {
+                            payment_date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                default:
+                    searchQuery = undefined;
+                    break;
+            }
+            return responses_1.default.fetch(res, yield payment_1.Payment.findAll(searchQuery));
         });
     }
     static createPayment(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.count("ha ha");
-            bookController_1.default.isAvailable(Number(req.body.book_id)).then((result) => {
-                if (!result)
-                    return res.json({ messge: "the book is unavilable" });
+            let isAvailable = true;
+            yield bookController_1.default.isAvailable(Number(req.body.book_id)).then((result) => {
+                if (!result) {
+                    isAvailable = false;
+                }
             });
+            if (!isAvailable)
+                return res.json({ messge: "the book is unavilable" });
             let staff = yield staff_1.Staff.findOne({
                 where: { credential_id: req.body.id },
             });
@@ -46,16 +107,6 @@ class PaymentController {
         });
     }
     static showPayment(req, res, next) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
-        });
-    }
-    static updatePayment(req, res, next) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
-        });
-    }
-    static deletePayment(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
             return res.json();
         });
--- src/app/controllers/paymentController.ts
+++ src/app/controllers/paymentController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { FindOptions, Op } from "sequelize";
 import { Payment } from "../models/payment";
 import { Staff } from "../models/staff";
 import { formatDate } from "../traits/formatDate";
@@ -11,7 +12,64 @@ class PaymentController {
         res: Response,
         next: NextFunction
     ): Promise<Response> {
-        return res.json();
+        let option = req.query.option;
+        let searchQuery: FindOptions<any> | undefined = undefined;
+        let start = new Date();
+        let end = new Date();
+        switch (option) {
+            case "today":
+                start.setHours(0, 0, 0, 0);
+                end.setHours(23, 59, 59, 59);
+                searchQuery = {
+                    where: {
+                        payment_date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "yesterday":
+                start.setHours(0, 0, 0, 0);
+                start.setDate(start.getDate() - 1);
+                end.setHours(23, 59, 59, 59);
+                end.setDate(end.getDate() - 1);
+                searchQuery = {
+                    where: {
+                        payment_date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "this_week":
+                start.setHours(0, 0, 0, 0);
+                end.setHours(23, 59, 59, 59);
+                end.setDate(end.getDate() + 7);
+                searchQuery = {
+                    where: {
+                        payment_date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "last_week":
+                start.setHours(0, 0, 0, 0);
+                end.setDate(end.getDate() - 7);
+                end.setHours(23, 59, 59, 59);
+                searchQuery = {
+                    where: {
+                        payment_date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            default:
+                searchQuery = undefined;
+                break;
+        }
+        return HttpResponse.fetch(res, await Payment.findAll(searchQuery));
     }
 
     static async createPayment(
@@ -19,10 +77,15 @@ class PaymentController {
         res: Response,
         next: NextFunction
     ) {
-        console.count("ha ha");
-        BookController.isAvailable(Number(req.body.book_id)).then((result) => {
-            if (!result) return res.json({ messge: "the book is unavilable" });
-        });
+        let isAvailable = true;
+        await BookController.isAvailable(Number(req.body.book_id)).then(
+            (result) => {
+                if (!result) {
+                    isAvailable = false;
+                }
+            }
+        );
+        if (!isAvailable) return res.json({ messge: "the book is unavilable" });
 
         let staff = await Staff.findOne({
             where: { credential_id: req.body.id },
@@ -41,22 +104,6 @@ class PaymentController {
     }
 
     static async showPayment(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response> {
-        return res.json();
-    }
-
-    static async updatePayment(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response> {
-        return res.json();
-    }
-
-    static async deletePayment(
         req: Request,
         res: Response,
         next: NextFunction
