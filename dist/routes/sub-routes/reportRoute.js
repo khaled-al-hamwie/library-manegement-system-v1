be454b9d8f9742158e50f6a7ec026513d3f4d2a4
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-11 21:58:55 +0300

    removing the param check

--- dist/routes/sub-routes/reportRoute.js
+++ dist/routes/sub-routes/reportRoute.js
@@ -5,12 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.reportRouterAdmin = void 0;
 const express_1 = require("express");
-const express_validator_1 = require("express-validator");
 const reportController_1 = __importDefault(require("../../app/controllers/reportController"));
 const validationHandler_1 = require("../../app/middleware/validationHandler");
 const reportValidator_1 = require("../../app/validators/reportValidator");
 exports.reportRouterAdmin = (0, express_1.Router)();
 exports.reportRouterAdmin
     .route("/report")
-    .get((0, express_validator_1.param)("name"), validationHandler_1.validationHandler, reportController_1.default.getReport)
+    .get(reportController_1.default.getReport)
     .post((0, reportValidator_1.reportValidator)(), validationHandler_1.validationHandler, reportController_1.default.createReport);
--- src/routes/sub-routes/reportRoute.ts
+++ src/routes/sub-routes/reportRoute.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { param } from "express-validator";
 import ReportController from "../../app/controllers/reportController";
 import { validationHandler } from "../../app/middleware/validationHandler";
 import { reportValidator } from "../../app/validators/reportValidator";
@@ -7,5 +6,5 @@ export const reportRouterAdmin: Router = Router();
 
 reportRouterAdmin
     .route("/report")
-    .get(param("name"), validationHandler, ReportController.getReport)
+    .get(ReportController.getReport)
     .post(reportValidator(), validationHandler, ReportController.createReport);
