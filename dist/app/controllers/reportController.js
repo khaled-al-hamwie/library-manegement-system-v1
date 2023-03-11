a1a85c9f982df757117362611168404873a92626
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-11 21:58:38 +0300

    adding the get reports

--- dist/app/controllers/reportController.js
+++ dist/app/controllers/reportController.js
@@ -8,16 +8,93 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const report_1 = require("../models/report");
+const staff_1 = require("../models/staff");
+const responses_1 = __importDefault(require("../traits/responses"));
 class ReportController {
     static getReport(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            let option = req.query.option;
+            let searchQuery = undefined;
+            let start = new Date();
+            let end = new Date();
+            switch (option) {
+                case "today":
+                    start.setHours(0, 0, 0, 0);
+                    end.setHours(23, 59, 59, 59);
+                    searchQuery = {
+                        where: {
+                            date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "yesterday":
+                    start.setHours(0, 0, 0, 0);
+                    start.setDate(start.getDate() - 1);
+                    end.setHours(23, 59, 59, 59);
+                    end.setDate(end.getDate() - 1);
+                    searchQuery = {
+                        where: {
+                            date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "this_week":
+                    start.setHours(0, 0, 0, 0);
+                    end.setHours(23, 59, 59, 59);
+                    end.setDate(end.getDate() + 7);
+                    searchQuery = {
+                        where: {
+                            date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                case "last_week":
+                    start.setHours(0, 0, 0, 0);
+                    end.setDate(end.getDate() - 7);
+                    end.setHours(23, 59, 59, 59);
+                    searchQuery = {
+                        where: {
+                            date: {
+                                [sequelize_1.Op.between]: [start, end],
+                            },
+                        },
+                    };
+                    break;
+                default:
+                    searchQuery = undefined;
+                    break;
+            }
+            return responses_1.default.fetch(res, yield report_1.Report.findAll(searchQuery));
         });
     }
     static createReport(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
-            return res.json();
+            let staff = yield staff_1.Staff.findOne({
+                where: { credential_id: req.body.id },
+            });
+            yield report_1.Report.create({
+                book_id: req.body.book_id,
+                reader_id: req.body.reader_id,
+                staff_id: Number(staff === null || staff === void 0 ? void 0 : staff.staff_id),
+                issue_id: req.body.issue_id,
+                reservation_id: req.body.reservation_id,
+                payment_id: req.body.payment_id,
+                report: req.body.report,
+            })
+                .then((result) => responses_1.default.creation(res, result, "report"))
+                .catch((err) => responses_1.default.server(res, err));
         });
     }
 }
--- src/app/controllers/reportController.ts
+++ src/app/controllers/reportController.ts
@@ -1,21 +1,86 @@
 import { NextFunction, Request, Response } from "express";
+import { FindOptions, Op } from "sequelize";
 import { Report } from "../models/report";
+import { Staff } from "../models/staff";
+import HttpResponse from "../traits/responses";
 
 class ReportController {
-    static async getReport(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response> {
-        return res.json();
+    static async getReport(req: Request, res: Response, next: NextFunction) {
+        let option = req.query.option;
+        let searchQuery: FindOptions<any> | undefined = undefined;
+        let start = new Date();
+        let end = new Date();
+        switch (option) {
+            case "today":
+                start.setHours(0, 0, 0, 0);
+                end.setHours(23, 59, 59, 59);
+                searchQuery = {
+                    where: {
+                        date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "yesterday":
+                start.setHours(0, 0, 0, 0);
+                start.setDate(start.getDate() - 1);
+                end.setHours(23, 59, 59, 59);
+                end.setDate(end.getDate() - 1);
+                searchQuery = {
+                    where: {
+                        date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "this_week":
+                start.setHours(0, 0, 0, 0);
+                end.setHours(23, 59, 59, 59);
+                end.setDate(end.getDate() + 7);
+                searchQuery = {
+                    where: {
+                        date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            case "last_week":
+                start.setHours(0, 0, 0, 0);
+                end.setDate(end.getDate() - 7);
+                end.setHours(23, 59, 59, 59);
+                searchQuery = {
+                    where: {
+                        date: {
+                            [Op.between]: [start, end],
+                        },
+                    },
+                };
+                break;
+            default:
+                searchQuery = undefined;
+                break;
+        }
+        return HttpResponse.fetch(res, await Report.findAll(searchQuery));
     }
 
-    static async createReport(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response> {
-        return res.json();
+    static async createReport(req: Request, res: Response, next: NextFunction) {
+        let staff = await Staff.findOne({
+            where: { credential_id: req.body.id },
+        });
+        await Report.create({
+            book_id: req.body.book_id,
+            reader_id: req.body.reader_id,
+            staff_id: Number(staff?.staff_id),
+            issue_id: req.body.issue_id,
+            reservation_id: req.body.reservation_id,
+            payment_id: req.body.payment_id,
+            report: req.body.report,
+        })
+            .then((result) => HttpResponse.creation(res, result, "report"))
+            .catch((err) => HttpResponse.server(res, err));
     }
 }
 export default ReportController;
